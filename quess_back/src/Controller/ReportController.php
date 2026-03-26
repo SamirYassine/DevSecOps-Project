9ef5ff6ade23ff017d5c007ee8bb3e283ec4e674
Yassine Samir <yassinesamir07@gmail.com>
2026-03-26 22:37:08 +0100

    fix: remove manual dompdf require and fix hardcoded path

--- quess_back/src/Controller/ReportController.php
+++ quess_back/src/Controller/ReportController.php
@@ -2,7 +2,7 @@
 
 
 namespace App\Controller;   
-require_once '../assets/dompdf/autoload.inc.php';
+//require_once '../assets/dompdf/autoload.inc.php';
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -468,7 +468,7 @@ $dompdf->render();
 $pdfContent = $dompdf->output();
 
 // Set the file path and name for saving the PDF on the server
-$outputDir = 'C:\Users\xxkil\Quess\quess_back\public\PDFs\\';
+$outputDir = $this->getParameter('kernel.project_dir') . '/public/PDFs/';
 $currentDateTime = date('Y-m-d H-i-s'); // Get current date and time
 $titlePdf = $orgName . '_Report_' . $currentDateTime;
 $pdfFilePath = $outputDir . str_replace(['é', 'ô'], ['e', 'o'], $titlePdf) . '.pdf';
