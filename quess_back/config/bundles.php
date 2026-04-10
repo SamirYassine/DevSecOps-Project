e2ec2a0c871a6d6b02e3c27b097c6e39d6fbbdde
Yassine Samir <yassinesamir07@gmail.com>
2026-04-10 19:25:52 +0200

    fix: remove TwigBundle from bundles.php (not needed for API-only backend)

--- quess_back/config/bundles.php
+++ quess_back/config/bundles.php
@@ -10,7 +10,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
